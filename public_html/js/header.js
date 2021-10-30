279daacb3e1d528cdf082163822b681560da6310
zhangdairui <464042992@qq.com>
2021-10-31 00:40:13 +0800

    10.31 finished all the pages

--- public_html/js/header.js
+++ public_html/js/header.js
@@ -1,6 +1,8 @@
-document.writeln("<div id=\'logo\'>logo</div>");
-document.writeln("<menu id=\'primaryLinks\'>");
-document.writeln("    <li><a href=\'./index.html\'>Home(Z-layout)</a></li>");
-document.writeln("    <li><a href=\'./time.html\'>TimeLine(F-layout)</a></li>");
-document.writeln("    <li><a href=\'./about.html\'>About(F-layout)</a></li>");
-document.writeln("</menu>");
+document.writeln("        <div id=\'logo\'><img src=\'image/logo_banner.png\' alt=\'\'></div>");
+document.writeln("        <menu id=\'primaryLinks\'>");
+document.writeln("            <ul>");
+document.writeln("            <li><a href=\'./index.html\'>Home</a></li>");
+document.writeln("            <li><a href=\'./time.html\'>TimeLine</a></li>");
+document.writeln("            <li><a href=\'./about.html\'>About</a></li>");
+document.writeln("            </ul>");
+document.writeln("        </menu>");
